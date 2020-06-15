cdab9f79c75407065f34588d11d35cead770fdc9
James Henager <jameschenager@gmail.com>
2020-06-15 15:43:20 -0700

    add business logic to make to do list objects and push list items values into them
    Co-Authored-By: Nikolas Kososik <nikolas.kososik@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,38 @@ function ToDoList() {
   this.currentId = 0;
 }
 
+ToDoList.prototype.addListItem = function(listItem) {
+  listItem.id = this.assignId();
+  this.listItems.push(listItem);
+}
+
+ToDoList.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+ToDoList.prototype.findListItem = function(id) {
+  for (let i=0; i< globalThis.listItems.length; i++) {
+    if (this.listItems[i]) {
+      if (this.listItems[i].id == id) {
+        return this.listItems[i];
+      }
+    }
+  };
+  return false;
+}
+
+ToDoList.prototype.deleteListItem = function(id) {
+  for (let i=0; i< globalThis.listItems.length; i++) {
+    if (this.listItems[i]) {
+      if (this.listItems[i].id == id) {
+        delete this.listItems[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
 
 
 
@@ -12,4 +44,6 @@ function ToDoList() {
 
 function listItem (itemName, details, timeFrame) {
   this.itemName = itemName;
+  this.details = details;
+  this.timeFrame - timeFrame;
 }
